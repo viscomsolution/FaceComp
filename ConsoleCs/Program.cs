810cce0fcdfa8691dcdbf81f0a3c04fc56527334
DESKTOP-BHDQH8H\vohungvi <vohungvi27@gmail.com>
2023-09-09 18:03:00 +0700

    + add console example

---
+++ ConsoleCs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGMTcs;
+
+namespace ConsoleCs
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+                return 0;
+
+            FaceComp detector = new FaceComp();
+
+            string imagePath1 = args[0];
+            string imagePath2 = args[1];
+
+            FacialCompare result = detector.Compare(imagePath1, imagePath2, true);
+            return result.percent;
+        }
+    }
+}
